3129519ff10e5cd02fd5a8ab6b77efe1e369fef4
Shivam Kumar <ydvshivam10@gmail.com>
2024-09-27 15:27:06 +0530

    updated typewriter effect

--- portfolio-site/src/Description.jsx
+++ portfolio-site/src/Description.jsx
@@ -13,9 +13,13 @@ export default function Description() {
 
             <div className="name">
                 <h4>
-                    <Typewriter onInit={(typewriter)=>{
-                        typewriter.typeString("Hey, I am Shivam Yadav").pauseFor(3000).start();
-                    }}/>
+                    <Typewriter
+                        options={{
+                            strings: "Hey I am Shivam Yadav",
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
                 </h4>
             </div>
             <div className="desc-outer">
@@ -24,7 +28,7 @@ export default function Description() {
                         I am a Full Stack <br />Web-Developer and I enjoy desigining websites.<br />
                     </h1>
                     <Button variant="contained" sx={{ backgroundColor: 'black', color: 'white', padding: '8px 16px', borderRadius: '45px', height: "60px", width: "150px", fontSize: "small" }}>
-                    <a href="https://www.linkedin.com/in/shivamkumar10/" style={{color:"inherit",textDecoration:"none"}} target="blank">Get in Touch</a> </Button>
+                        <a href="https://www.linkedin.com/in/shivamkumar10/" style={{ color: "inherit", textDecoration: "none" }} target="blank">Get in Touch</a> </Button>
                 </div>
                 <div className="outer-profile-pic">
                     <img src={DP} alt="Profile-pic" className="profile-img" />
